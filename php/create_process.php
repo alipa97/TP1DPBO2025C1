371c9ab46cea3214cd5dfec9931a049ee69da4e2
Alifa Salsabila <153807422+alipa97@users.noreply.github.com>
2025-02-23 10:07:22 +0700

    Update create_process.php

--- php/create_process.php
+++ php/create_process.php
@@ -2,21 +2,23 @@
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Menambah data dari form input
     $nama = $_POST['nama'];
     $kategori = $_POST['kategori'];
     $harga = $_POST['harga'];
-    
     $foto = $_FILES['foto']['name'];
-    // var_dump($foto);
-    // die(); // Stop eksekusi
-    // dd($foto);
+
+    // Memindahkan file gambar ke folder 'images'
     move_uploaded_file($_FILES['foto']['tmp_name'], "images/".$foto);
 
+    // membuat data baru
     $id_baru = count($_SESSION['produk']) + 1;
     $produk_baru = ["id" => $id_baru, "nama" => $nama, "kategori" => $kategori, "harga" => $harga, "foto" => $foto];
 
+    // menyimpan produk ke sesi
     $_SESSION['produk'][] = $produk_baru;
 
+    // kembali ke laman awal
     header("Location: index.php");
 }
 ?>
